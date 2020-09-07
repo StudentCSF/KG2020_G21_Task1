9e2a4773750fd681a8a65b2d1d0b25b13019ffc4
Victor Valeev <viktorval2001@gmail.com>
2020-09-07 21:28:00 +0300

    Update the picture.

--- src/ru/vsu/cs/valeev/Home.java
+++ src/ru/vsu/cs/valeev/Home.java
@@ -9,6 +9,7 @@ public class Home {
     WindowHome window;
     Roof roof;
     Chimnei chim;
+    StreetTable st;
 
     private class Door {
         int width, height;
@@ -24,7 +25,7 @@ public class Home {
             int y = Home.this.y;
             int w = Home.this.width;
             int h = Home.this.wallHeight;
-            g.fillArc(x + w * 2 / 3, y + h / 8, this.width, this.height, 0 ,180);
+            g.fillArc(x + w * 2 / 3, y + h / 8, this.width, this.height, 0, 180);
             g.setColor(Color.BLACK);
             g.fillRect(x + w * 3 / 4 - this.width / 5, y + 5 * h / 8, 10, 3);
         }
@@ -54,24 +55,24 @@ public class Home {
     }
 
     private class Roof {
-       int height;
+        int height;
 
-       public Roof(int height) {
-           this.height = height;
-       }
+        public Roof(int height) {
+            this.height = height;
+        }
 
-       public void draw(Graphics2D g) {
-           g.setColor(Home.this.roofColor);
+        public void draw(Graphics2D g) {
+            g.setColor(Home.this.roofColor);
 
-           int x = Home.this.x;
-           int y = Home.this.y;
-           int w = Home.this.width;
-           int[] a1 = {x - w / 10, x + w / 2, x + w + w / 10};
-           int[] a2 = {y, y - this.height, y};
+            int x = Home.this.x;
+            int y = Home.this.y;
+            int w = Home.this.width;
+            int[] a1 = {x - w / 10, x + w / 2, x + w + w / 10};
+            int[] a2 = {y, y - this.height, y};
 
-           g.fillPolygon(a1, a2, 3);
+            g.fillPolygon(a1, a2, 3);
 
-       }
+        }
     }
 
     private class Chimnei {
@@ -79,7 +80,31 @@ public class Home {
         public void draw(Graphics2D g) {
             g.setColor(Home.this.roofColor);
             g.fillRect(Home.this.x + Home.this.width / 7, Home.this.y - Home.this.wallHeight / 10 - 300, 50, 300);
-           // g.fillRect();
+            // g.fillRect();
+        }
+    }
+
+    private class StreetTable {
+        int x, y, width, height;
+        Color color;
+        String name;
+
+        public StreetTable(int x, int y, int width, int height, Color color, String name) {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+            this.color = color;
+            this.name = name;
+        }
+
+        public void draw(Graphics2D g) {
+            g.setColor(this.color);
+            g.fillRect(this.x, this.y, this.width, this.height);
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("name", 5, 20));
+            char[] c = this.name.toCharArray();
+            g.drawChars(c, 0, c.length, this.x + 5, this.y + this.height - 10);
         }
     }
 
@@ -105,10 +130,13 @@ public class Home {
         this.chim = new Chimnei();
         this.chim.draw(g);
 
-        this.door = new Door(width / 4, wallHeight  * 14 / 8);
+        this.door = new Door(width / 4, wallHeight * 14 / 8);
         door.draw(g);
 
         this.window = new WindowHome(this.x + this.width / 5, this.y + this.wallHeight / 2, 80, 8);
         this.window.draw(g);
+
+        this.st = new StreetTable(this.x + this.width / 20, this.y + this.wallHeight / 12, 150, 40, new Color(72, 118, 255), "Java Street, 1");
+        this.st.draw(g);
     }
 }
